e9b04aaaeecab19556c961c3f01d0540ad3761bc
rambod the coder <167572766+rambodi89@users.noreply.github.com>
2024-04-23 17:54:39 +0330

    Add files via upload

---
+++ login.py
@@ -0,0 +1,8 @@
+emailogin1="jedimifarmayin"
+emailogin=(input('email plz'))
+passlogin=input('pass plz')
+while emailogin==emailogin1 :
+    if passlogin==123456789 :
+        print('welcome back')
+    else :
+        print('email or password is incorrect')
